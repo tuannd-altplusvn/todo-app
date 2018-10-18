921e6efcb1b6949f467f8a9e66c9e83ed1cd6a41
Nguyen Duy Tuan <nguyenduytuan@APV-PC170.local>
2018-10-18 20:25:10 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="app">
+    <todo-list v-bind:todos="todos"></todo-list>
+    </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+export default {
+    name: 'App',
+    components: {
+        TodoList
+    },
+    data() {
+        return {
+            todos: [
+                {
+                    title: 'Todo A',
+                    project: 'Project A',
+                    done: false
+                },
+                {
+                    title: 'Todo B',
+                    project: 'Project B',
+                    done: false
+                },
+                {
+                    title: 'Todo C',
+                    project: 'Project C',
+                    done: true
+                },
+                {
+                    title: 'Todo D',
+                    project: 'Project D',
+                    done: false
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<p>Complete Tasks: {{ todos.filter(todo => { return todo.done === true }).length }}</p>
+		<p>Pending Tasks: {{ todos.filter(todo => { return todo.done === false }).length }}</p>
+		<hr>
+		<todo v-for="(todo, index) in todos" v-bind:todo="todo" v-bind:index="index"></todo>
+	</div>
+</template>
+
+<script>
+	import Todo from '@/components/Todo'
+
+	export default {
+		props: ['todos'],
+		components: {
+			Todo,
+		}
+	}
+</script>
+
+<style type="scss" scoped>
+	
+</style>
